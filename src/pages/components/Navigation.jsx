b65968cb016f1f58b279a5e8c0ac7ffc1db265de
NejahA <achref.nejah@gmail.com>
2024-06-11 19:12:03 +0000

    added resume

--- src/pages/components/Navigation.jsx
+++ src/pages/components/Navigation.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import styles from "./Navigation.module.css";
 import { useNavigate } from 'react-router-dom';
+import { IoMdDownload } from "react-icons/io";
 
 const Navigation = () => {
   const nav =  useNavigate()
@@ -12,6 +13,12 @@ const Navigation = () => {
             <div className={styles.about} onClick={()=>nav("/about")}>About</div>
             <div className={styles.projects} onClick={()=>nav("/projects")}>Projects</div>
             <div className={styles.contact} onClick={()=>nav("/contact")}>Contact</div>
+            <a className={styles.resume}
+             href='/Achref-Nejah-Resume.pdf' target="_blank"
+             download="Achref-Nejah-Resume.pdf">
+            <IoMdDownload className={styles.download}/>
+              Resume
+            </a>
           </nav>
         </div>
       </div>  )
